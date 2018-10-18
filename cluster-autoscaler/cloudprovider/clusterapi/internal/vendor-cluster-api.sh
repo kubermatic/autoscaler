c4866437be460a16233888ddcedfaf77414d704f
Andrew McDermott <aim@frobware.com>
2018-10-18 12:05:25 +0100

    Update clusterapi-vendor script
    Accommodate changes to upstream vendor layout.

--- cluster-autoscaler/cloudprovider/clusterapi/internal/vendor-cluster-api.sh
+++ cluster-autoscaler/cloudprovider/clusterapi/internal/vendor-cluster-api.sh
@@ -3,9 +3,10 @@
 rm -rf vendor
 mkdir vendor
 cd vendor
-mkdir sigs.k8s.io
-(cd sigs.k8s.io; git clone --depth=1 https://github.com/kubernetes-sigs/cluster-api)
-mv sigs.k8s.io/cluster-api/vendor/* .
+mkdir new-sigs.k8s.io
+(cd new-sigs.k8s.io; git clone --depth=1 https://github.com/kubernetes-sigs/cluster-api)
+mv new-sigs.k8s.io/cluster-api/vendor/* .
+mv new-sigs.k8s.io/* sigs.k8s.io/
 rmdir sigs.k8s.io/cluster-api/vendor
 
 # clean up duplicates which result in panics at runtime
